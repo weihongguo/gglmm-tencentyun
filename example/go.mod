5c2cfddfe1c8008f2d75b33a6ddf1eca12a721b4
weihongguo <35117668@qq.com>
2020-02-27 14:57:33 +0800

    example

---
+++ example/go.mod
@@ -0,0 +1,10 @@
+module gglmm-tencentyun-example
+
+go 1.13
+
+replace github.com/weihongguo/gglmm-tencentyun => ../
+
+require (
+	github.com/weihongguo/gglmm v0.0.0-20200226150144-384f169aa64a
+	github.com/weihongguo/gglmm-tencentyun v0.0.0-20200227064302-529afe0d0cae
+)
---
+++ example/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"mime/multipart"
+	"net/http"
+
+	"github.com/weihongguo/gglmm"
+	tencentyun "github.com/weihongguo/gglmm-tencentyun"
+)
+
+func main() {
+	gglmm.RegisterRedisCacher("tcp", "127.0.0.1:6379", 10, 5, 3)
+	defer gglmm.CloseRedisCacher()
+
+	gglmm.RegisterBasePath("/api/example")
+
+	gglmm.RegisterHTTPHandler(tencentyun.NewCosCredentialsService("secretID", "secretKey", "region", "appID", "bucket", cosPrefixKey), "/cos")
+
+	gglmm.RegisterHTTPHandler(tencentyun.NewCosUploadService("secretID", "secretKey", "region", "appID", "bucket", cosKeyFile), "/cos")
+
+	gglmm.ListenAndServe(":10000")
+}
+
+func cosPrefixKey(r *http.Request) (string, error) {
+	// 根据请求计算路径前缀
+	return "example", nil
+}
+
+func cosKeyFile(r *http.Request) (string, multipart.File, error) {
+	file, _, err := r.FormFile("example")
+	if err != nil {
+		return "", nil, err
+	}
+
+	// 其他判断
+
+	return "example", file, nil
+}
